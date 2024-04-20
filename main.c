1c1e7f9cdff4f67e310ee50cbd45256e3d6a7f07
c0utin <rafaelcouto111@gmail.com>
2024-04-20 16:00:16 -0300

    feat: project sample to screen

--- main.c
+++ main.c
@@ -2,12 +2,30 @@
 
 #include <raylib.h>
 
+#define MIN_X -20.0
+#define MAX_X 20.0
+#define MIN_Y -20.0
+#define MAX_Y 20.0
+
+Vector2 projectSampleToScreen(Vector2 sample){
+
+  float w = GetScreenWidth();
+  float h = GetScreenHeight();
+  return CLITERAL(Vector2){
+    .x = (sample.x - MIN_X)/(MAX_X - MIN_X)* w,
+    .y = h - (sample.y - MIN_Y)/(MAX_Y - MIN_Y)* h,
+  };
+
+}
+
 int main(void){
 
-  InitWindow(800, 600, "cu");
+  InitWindow(800, 600, "c-means");
   while(!WindowShouldClose()){
     BeginDrawing();
-    ClearBackground(RED);
+    ClearBackground(GetColor(0x181818AA));
+    Vector2 sample = {-10, -10};
+    DrawCircleV(projectSampleToScreen(sample), 5, RED);
     EndDrawing();
   }
   CloseWindow();
